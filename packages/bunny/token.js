a1bc76f4ad2e19238c9f9c5b4fc3548098aacf7e
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-01-09 10:56:22 +1100

    formatting

--- packages/bunny/token.js
+++ packages/bunny/token.js
@@ -4,24 +4,22 @@
 
 async function sha256(str) {
     const text = new TextEncoder().encode(str)
-    const hashBuffer = await crypto.subtle.digest("SHA-256", text)
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    console.log('sha256', hashHex)
-    return btoa(hashHex)
+    const hashBuffer = await crypto.subtle.digest('SHA-256', text)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('') // convert to hex
 }
 
 function addCountries(url, a, b) {
-    var tempUrl = url;
-    if (a != null && a != "") {
-        var tempUrlOne = new URL(tempUrl);
-        tempUrl += ((tempUrlOne.search == "") ? "?" : "&") + "token_countries=" + a;
+    var tempUrl = url
+    if (a != null && a != '') {
+        var tempUrlOne = new URL(tempUrl)
+        tempUrl += (tempUrlOne.search == '' ? '?' : '&') + 'token_countries=' + a
     }
-    if (b != null && b != "") {
-        var tempUrlTwo = new URL(tempUrl);
-        tempUrl += ((tempUrlTwo.search == "") ? "?" : "&") + "token_countries_blocked=" + b;
+    if (b != null && b != '') {
+        var tempUrlTwo = new URL(tempUrl)
+        tempUrl += (tempUrlTwo.search == '' ? '?' : '&') + 'token_countries_blocked=' + b
     }
-    return tempUrl;
+    return tempUrl
 }
 
 /**
@@ -36,16 +34,19 @@ function addCountries(url, a, b) {
  * @param {string} countriesBlocked
  */
 async function signUrl(url, securityKey, expirationTime = 3600, userIp, isDirectory = false, pathAllowed, countriesAllowed, countriesBlocked) {
-    var parameterData = "", parameterDataUrl = "", signaturePath = "", hashableBase = "", token = "";
-    var expires = Math.floor(new Date() / 1000) + expirationTime;
-    var url = addCountries(url, countriesAllowed, countriesBlocked);
-    var parsedUrl = new URL(url);
-    var parameters = (new URL(url)).searchParams;
+    var parameterData = '',
+        parameterDataUrl = '',
+        signaturePath = '',
+        hashableBase = '',
+        token = ''
+    var expires = Math.floor(new Date() / 1000) + expirationTime
+    var url = addCountries(url, countriesAllowed, countriesBlocked)
+    var parsedUrl = new URL(url)
+    var parameters = parsedUrl.searchParams
+    signaturePath = parsedUrl.pathname
     if (pathAllowed != null) {
-        signaturePath = pathAllowed;
-        parameters.set("token_path", pathAllowed);
-    } else {
-        signaturePath = parsedUrl.pathname;
+        signaturePath = pathAllowed
+        parameters.set('token_path', pathAllowed)
     }
     parameters.sort()
     console.log('parameters')
@@ -54,21 +55,27 @@ async function signUrl(url, securityKey, expirationTime = 3600, userIp, isDirect
         let value = pair[1]
         console.log(key, value)
         if (parameterData.length > 0) {
-            parameterData += "&";
+            parameterData += '&'
         }
-        parameterData += key + "=" + value;
-        parameterDataUrl += "&" + key + "=" + encodeURIComponent(value);
+        parameterData += `${key}=${value}`
+        parameterDataUrl += `&${key}=` + encodeURIComponent(value)
     }
-    console.log('parameterDataUrl', parameterDataUrl);
-    hashableBase = securityKey + signaturePath + expires + ((userIp != null) ? userIp : "") + parameterData;
-    console.log('hashableBase', hashableBase);
-    token = await sha256(hashableBase);
-    token = token.replace(/\n/g, "").replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+    console.log('parameterDataUrl', parameterDataUrl)
+    hashableBase = securityKey + signaturePath + expires + (userIp != null ? userIp : '') + parameterData
+    console.log('hashableBase', hashableBase)
+    token = await sha256(hashableBase)
+    console.log('sha256', token)
+    token = btoa(token)
+        .replace(/\n/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '')
+    console.log('btoa', token)
     if (isDirectory) {
-        return parsedUrl.protocol + "//" + parsedUrl.host + "/bcdn_token=" + token + parameterDataUrl + "&expires=" + expires + parsedUrl.pathname;
+        return parsedUrl.protocol + '//' + parsedUrl.host + '/bcdn_token=' + token + parameterDataUrl + '&expires=' + expires + parsedUrl.pathname
     } else {
-        return parsedUrl.protocol + "//" + parsedUrl.host + parsedUrl.pathname + "?token=" + token + parameterDataUrl + "&expires=" + expires;
+        return parsedUrl.protocol + '//' + parsedUrl.host + parsedUrl.pathname + '?token=' + token + parameterDataUrl + '&expires=' + expires
     }
 }
 
-export default signUrl;
+export default signUrl
